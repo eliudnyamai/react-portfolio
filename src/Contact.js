01f435969c3e59d449e37c55c9eed22b46849270
eliudnyamai <eliudmitau@gmail.com>
2021-03-05 14:34:32 +0300

    Deployment File Config

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './contact.css'
 import  emailjs from 'emailjs-com'
+import ReCAPTCHA from "react-google-recaptcha";
 class Contact extends React.Component {
   constructor() {
     super();
@@ -8,9 +9,16 @@ class Contact extends React.Component {
       name: "",
       email: "",
       message: "",
+      feedback:"",
+      recaptcha:false
     };
     this.handleChange = this.handleChange.bind(this);
     this.sendFeedback=this.sendFeedback.bind(this);
+    this.handleRecaptcha=this.handleRecaptcha(this);
+  }
+  handleRecaptcha(){
+   this.setState({recaptcha:true})
+   console.log(this.state.recaptcha)
   }
   handleChange(event) {
     const { name, value } = event.target;
@@ -19,27 +27,39 @@ class Contact extends React.Component {
     });
   }
   sendFeedback() {
-    emailjs.send("service_8e58g0f","template_sajk7y9",{
-           to_name: "eliud",
-           from_name: this.state.name,
-            message: this.state.message,
-              },'user_7HIgS9Fq3JCScYmOeE86l')
+    var variables = {
+      to_name: "eliud",
+      from_name: this.state.name,
+      message: this.state.message,
+    };
+    emailjs.send("service_8e58g0f","template_sajk7y9",variables,'user_7HIgS9Fq3JCScYmOeE86l')
       .then((res) => {
-        console.log("Email successfully sent!");
+        this.setState({
+          feedback: "Thanks I have received your message",
+          email: "",
+          message:"",
+          name:""
+        });
       })
       // Handle errors here however you like, or use a React error boundary
-      .catch((err) =>
+      .catch((err) =>{
+        this.setState({ feedback: "Message not send please check your internet connection" });
         console.error(
           "Oh well, you failed. Here some thoughts on the error that occured:",
           err
-        )
+        );
+      }
       );
   }
 
   handleSubmit=()=> {
     console.log('yes')
-    var variables = {to_name:"Eliud",from_name:this.state.name,message:this.state.message};
-    this.sendFeedback();
+    alert(this.state.message)
+    if(this.state.message&&this.state.name&&this.state.name){
+        this.sendFeedback();
+    } else{
+      this.setState({feedback:"All fields are required"})
+    }
   }
 
   render() {
@@ -47,9 +67,9 @@ class Contact extends React.Component {
       <div className="row bg-dark text-white py-5">
         <div id="contact" className="col-md-6 text-center p-2 shadow mx-auto ">
           <h4>Send Me a message</h4>
-          <form >
+          <form>
             <input
-              className="form-control text-center col-sm-10 ml-5"
+              className="form-control text-center col-8 mx-auto"
               type="text"
               value={this.state.firstName}
               name="name"
@@ -59,7 +79,7 @@ class Contact extends React.Component {
             />
             <br />
             <input
-              className="form-control text-center col-sm-10 ml-5"
+              className="form-control col-8 text-center  mx-auto"
               type="email"
               value={this.state.email}
               name="email"
@@ -69,7 +89,7 @@ class Contact extends React.Component {
             />
             <br />
             <textarea
-              className="form-control text-center col-sm-10 ml-5"
+              className="form-control text-center col-8 mx-auto"
               value={this.state.message}
               name="message"
               placeholder="Type Your Message"
@@ -77,18 +97,27 @@ class Contact extends React.Component {
               onChange={this.handleChange}
             />
             <br />
-            <input
-              onClick={this.handleSubmit}
-              className="btn button btn-large btn-success"
-              type="button"
-              value="Send"
-            />
+            <div className="mx-auto d-md-flex col-10 text-center">
+              <ReCAPTCHA
+                className=" px-5 "
+                sitekey="6Lcxi8EUAAAAABwrCyKCoJmbZMeNUrTI1K5jm-TG"
+                onChange={this.handleRecaptcha}
+              />
+              <input
+                onClick={this.handleSubmit}
+                className="mt-2 mt-md-4 btn button btn-large btn-success"
+                type="button"
+                value="Send"
+              />
+            </div>
+            <br />
+
+            <div className="text-info h5">{this.state.feedback}</div>
           </form>
         </div>
-        <div className="social  mx-auto col-md-4">
-          <div>
-            <h3>Social</h3>
-            <div className="mb-2">
+        <div className="social text-center  mx-auto col-md-4">
+            <h3 className="mt-1">Social</h3>
+            <div className="">
               <i className=" icon  fab fa-twitter"></i>
               <span className="h5">&nbsp;Follow me at</span>
               <a className="h5" href="https://twitter.com/MitauNyamai">
@@ -119,7 +148,7 @@ class Contact extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+     
     );
   }
 }
